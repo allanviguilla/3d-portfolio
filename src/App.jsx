33b124a60f2d3771d405ac626217603a00ddac58
Allan Viguilla <allanviguilla@Allans-MacBook-Pro.local>
2023-12-04 20:31:46 -0800

    Create and style navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
-      <main className="bg-slate-300/20">
-        <Router>
-          Navbar
-          <Routes>
-            <Route path="/" element={"Home"} />
-            <Route path="/about" element={"About"} />
-            <Route path="/work" element={"Work"} />
-            <Route path="/contact" element={"Contact"} />
-          </Routes>
-        </Router>
-      </main>
-    </>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/work" element={"Work"} />
+          <Route path="/contact" element={"Contact"} />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">AV</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/work"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Work
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
