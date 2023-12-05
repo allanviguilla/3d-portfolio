007771b18db436e8b5cdd87638d75ac365221be3
Allan Viguilla <allanviguilla@Allans-MacBook-Pro.local>
2023-12-04 22:04:40 -0800

    Create home page, loader component, and add models to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import { Home, About, Work, Contact } from "./pages";
 
 const App = () => {
   return (
@@ -8,10 +9,10 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/work" element={"Work"} />
-          <Route path="/contact" element={"Contact"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
